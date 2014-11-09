b9e586d9c970c26493af31e38b7fc568e072f0f1
mitov90 <mitov90@gmail.com>
2014-11-09 12:58:56 +0200

    removed packages from gitignore

---
+++ Web/Yashmak.Web/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿namespace Yashmak.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Microsoft.AspNet.Identity;
+
+    using Yashmak.Data;
+    using Yashmak.Data.Models;
+
+    public class FilesController : Controller
+    {
+        private readonly IYashmakData dbContext;
+
+        public FilesController(IYashmakData data)
+        {
+            this.dbContext = data;
+        }
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(File file)
+        {
+            if (this.ModelState.IsValid)
+            {
+                file.UserId = User.Identity.GetUserId();
+                this.dbContext.Files.Add(file);
+                this.dbContext.SaveChanges();
+            }
+
+            return this.View(file);
+        }
+    }
+}
---
+++ Yashmak.Common/FileSystemHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Yashmak.Common
+{
+    using System.IO;
+
+    public static class FileSystemHelper
+    {
+        private const string DataFolder = "/App_Data/";
+
+        public static bool CreateFolder(string path, string newFolderName)
+        {
+            string newDirPath = path + "/" + newFolderName;
+
+            bool exists = Directory.Exists(newDirPath);
+            if (!exists)
+            {
+                Directory.CreateDirectory(newDirPath);
+            }
+
+            return Directory.Exists(newDirPath);
+        }
+
+        public static bool CreateUserFolder(string path, string newFolderName)
+        {
+            string newDirPath = path + DataFolder + newFolderName;
+
+            bool exists = Directory.Exists(newDirPath);
+            if (!exists)
+            {
+                Directory.CreateDirectory(newDirPath);
+            }
+
+            return Directory.Exists(newDirPath);
+        }
+    }
+}
