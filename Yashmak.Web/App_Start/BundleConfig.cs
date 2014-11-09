92f119188783ab2fe9c70f69d3bb8d1748edf373
mitov90 <mitov90@gmail.com>
2014-11-09 14:35:18 +0200

    Added flatly bootstrap theme

--- Yashmak.Web/App_Start/BundleConfig.cs
+++ Yashmak.Web/App_Start/BundleConfig.cs
@@ -7,17 +7,18 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery")
-                .Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
-                .Include("~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css")
-                .Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(
+                new StyleBundle("~/Content/css").Include(
+                    "~/Content/bootstrap.css", 
+                    "~/Content/bootstrap.flatly.css", 
+                    "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
