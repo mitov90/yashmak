d3bdd2af072e114955b1079ea838677a8b925410
mitov90 <mitov90@gmail.com>
2014-11-10 11:12:31 +0200

    Dynamic select file/directory

---
+++ Yashmak.Web/Scripts/fileNode.select.js
@@ -0,0 +1,10 @@
+﻿$("body").on("click", ".file-row", function (ev) {
+    var self = ($(this));
+    if (self.hasClass("success")) {
+        self.removeClass("success");
+        self.prev().addClass("hidden");
+    } else {
+        self.addClass("success");
+        self.prev().removeClass("hidden");
+    }
+});
