4b9888e3d77e44ba453f87acbdd37dbb5a3e61b9
mitov90 <mitov90@gmail.com>
2014-11-11 01:24:22 +0200

    File upload working

--- Yashmak.Web/App_Start/BundleConfig.cs
+++ Yashmak.Web/App_Start/BundleConfig.cs
@@ -4,48 +4,36 @@
 
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             RegisterScripts(bundles);
-
             RegisterStyles(bundles);
 
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/site").Include(
-                "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/site.css"));
 
             bundles.Add(
                 new StyleBundle("~/Content/css").Include(
-                    "~/Content/bootstrap.css",
-                    "~/Content/bootstrap.flatly.css",
+                    "~/Content/bootstrap.css", 
+                    "~/Content/bootstrap.flatly.css", 
                     "~/Content/font-awesome.css"));
-
-            bundles.Add(
-                new StyleBundle("~/Content/dropzone").Include(
-                    "~/Scripts/dropzone/css/basic.css",
-                    "~/Scripts/dropzone/css/dropzone.css"));
         }
 
         private static void RegisterScripts(BundleCollection bundles)
         {
             bundles.Add(
                 new ScriptBundle("~/bundles/jquery").Include(
-                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/jquery-{version}.js", 
                     "~/Scripts/jquery.unobtrusive-ajax.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/dropzone").Include("~/Scripts/dropzone/dropzone.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
         }
     }
 }
--- Yashmak.Web/Controllers/UploadController.cs
+++ Yashmak.Web/Controllers/UploadController.cs
@@ -1,9 +1,20 @@
 ﻿namespace Yashmak.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
     using System.Web.Mvc;
 
+    using Microsoft.AspNet.Identity;
+
+    using MvcFileUploader;
+    using MvcFileUploader.Models;
+
     using Yashmak.Data.Common.Repository;
-    using Yashmak.Data.Models;
+
+    using File = Yashmak.Data.Models.File;
 
     public class UploadController : Controller
     {
@@ -14,9 +25,131 @@
             this.repository = repository;
         }
 
-        public ActionResult FileUpload(int filenodeid)
+        [HttpGet]
+        public ActionResult FileUpload(int? filenodeid)
         {
-            throw new System.NotImplementedException();
+            return this.PartialView(filenodeid);
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(int? filenodeid, string test)
+        {
+            var username = this.User.Identity.Name;
+            var savePath = Server.MapPath(string.Format(@"~/App_Data/{0}/", username));
+            var userId = this.User.Identity.GetUserId();
+            var userFilesInCurrentDir = this.repository.All().Where(d => d.ParentId == filenodeid && d.UserId == userId);
+
+            var statuses = new List<ViewDataUploadFileResult>();
+
+            for (var i = 0; i < this.Request.Files.Count; i++)
+            {
+                var curFile = i;
+                var curFileName = this.Request.Files[curFile].FileName;
+                var st = FileSaver.StoreFile(
+                    x =>
+                    {
+                        x.File = this.Request.Files[curFile];
+
+                        // note how we are adding an additional value to be posted with delete request
+                        // and giving it the same value posted with upload
+                        x.DeleteUrl = this.Url.Action("DeleteFile", new { filenodeid = filenodeid });
+                        x.StorageDirectory = savePath;
+                        x.UrlPrefix = string.Format("/App_Data/{0}", username);// this is used to generate the relative url of the file
+                        // overriding defaults
+                        x.FileName = curFileName;
+                        
+                        // default is filename suffixed with filetimestamp
+                        x.ThrowExceptions = true;
+
+                        // default is false, if false exception message is set in error property
+                    });
+
+                if (userFilesInCurrentDir.Any(f => f.FileName == curFileName))
+                {
+                    st.error = "This name alredy exists in folder!";
+                }
+                else
+                {
+                    this.repository.Add(new File
+                                            {
+                                                FileName = curFileName,
+                                                IsDirectory = false,
+                                                ModifiedOn = DateTime.Now,
+                                                ParentId = filenodeid,
+                                                UserId = userId,
+                                                Size = st.size,
+                                                PathToFile = st.SavedFileName
+                                            });
+                    this.repository.SaveChanges();
+                }
+
+                statuses.Add(st);
+            }
+
+            //statuses.ForEach(x => x.thumbnailUrl = x.url + "?width=80&height=80");
+            //statuses.ForEach(x => x.url = this.Url.Action("DownloadFile", new { fileUrl = x.url, mimetype = x.type }));
+            
+            if (filenodeid == 13)
+            {
+                var rnd = new Random();
+                statuses.ForEach(
+                    x =>
+                    {
+                        // setting the error property removes the deleteUrl, thumbnailUrl and url property values
+                        x.error = rnd.Next(0, 2) > 0
+                                      ? "We do not have any entity with unlucky Id : '13'"
+                                      : string.Format("Your file size is {0} bytes which is un-acceptable", x.size);
+
+                        // delete file by using FullPath property
+                        if (System.IO.File.Exists(x.FullPath))
+                        {
+                            System.IO.File.Delete(x.FullPath);
+                        }
+                    });
+            }
+
+            var viewresult = this.Json(new { files = statuses });
+
+            // for IE8 which does not accept application/json
+            if (this.Request.Headers["Accept"] != null && !this.Request.Headers["Accept"].Contains("application/json"))
+            {
+                viewresult.ContentType = "text/plain";
+            }
+
+            return viewresult;
+        }
+
+        // here i am receving the extra info injected
+        [HttpPost] // should accept only post
+        public ActionResult DeleteFile(int? filenodeid, string fileUrl)
+        {
+            var filePath = this.Server.MapPath("~" + fileUrl);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            var viewresult = this.Json(new { error = string.Empty });
+
+            // for IE8 which does not accept application/json
+            if (this.Request.Headers["Accept"] != null && !this.Request.Headers["Accept"].Contains("application/json"))
+            {
+                viewresult.ContentType = "text/plain";
+            }
+
+            return viewresult; // trigger success
+        }
+
+        public ActionResult DownloadFile(string fileUrl, string mimetype)
+        {
+            var filePath = this.Server.MapPath("~" + fileUrl);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return File(filePath, mimetype);
+            }
+            return new HttpNotFoundResult("File not found");
         }
     }
 }
