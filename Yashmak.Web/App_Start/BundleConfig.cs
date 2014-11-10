a42fdf4a84104da1c54e525142e1a30ffab7b016
mitov90 <mitov90@gmail.com>
2014-11-10 12:39:04 +0200

    Using font-awesome

--- Yashmak.Web/App_Start/BundleConfig.cs
+++ Yashmak.Web/App_Start/BundleConfig.cs
@@ -18,6 +18,7 @@
                 new StyleBundle("~/Content/css").Include(
                     "~/Content/bootstrap.css", 
                     "~/Content/bootstrap.flatly.css", 
+                    "~/Content/font-awesome.css", 
                     "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
