25889d226ee9690b1890afd604c631670777187d
mitov90 <mitov90@gmail.com>
2014-11-11 04:28:26 +0200

    Basic seach func

---
+++ Yashmak.Web/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿namespace Yashmak.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using Microsoft.AspNet.Identity;
+
+    using Yashmak.Data.Common.Repository;
+    using Yashmak.Data.Models;
+    using Yashmak.Web.Models.Directory;
+
+    public class SearchController : Controller
+    {
+        private readonly IDeletableEntityRepository<File> repository;
+
+        public SearchController(IDeletableEntityRepository<File> repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Search(string query)
+        {
+            var userId = this.User.Identity.GetUserId();
+            var queryable =
+                this.repository.All()
+                    .Where(f => f.UserId == userId && f.FileName.Contains(query))
+                    .Project()
+                    .To<FileViewModel>();
+
+            return this.View(queryable);
+        }
+    }
+}
